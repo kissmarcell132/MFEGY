beb2a0ce9eca3e2d3adf8a140b5d314b11b0a5ba
kissmarcell132 <72729160+kissmarcell132@users.noreply.github.com>
2021-10-14 12:47:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ def main() -> None:
     if a != 0:
         if math.pow(b) >= 4 * a * c:
             if math.pow(b) == 4 * a * c:
-                x1 = (-b + math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
-                x2 = (-b - math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+                x1 = (-b + math.pow(b ** 2 - 4 * a * c)) / (2 * a)
+                x2 = (-b - math.pow(b ** 2 - 4 * a * c)) / (2 * a)
                 print(f'x1 = {x1}')
                 print(f'x2 = {x2}')
             else:
